dcfef60e4fb79390cd45c187f9c6ebefca97174b
DShokhov <dmytro_shohov@charityhero.live>
2025-08-30 15:16:01 +0200

    Добавьте файлы проекта.

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDBContext context) : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyCollection<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToArrayAsync().ConfigureAwait(false);
+
+            return members ;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await context.Users.FindAsync(id).ConfigureAwait(false);
+            if(member is null ) return NotFound();
+            return member ;
+        }
+
+    }
+}
---
+++ API/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users {  get; set; }
+    }
+}
